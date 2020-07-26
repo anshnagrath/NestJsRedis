09c775cf15a593b059ffd026b5b36bae8acfa39c
Ansh Nagrath <you@example.com>
2020-07-26 20:09:51 +0530

    Added dotenv

--- microservices/client/src/app.module.ts
+++ microservices/client/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ClientSchema } from './schemas/client.schema'; 
-
+import { config } from 'dotenv' ;
+config();
 @Module({
-  imports: [   MongooseModule.forRoot('mongodb://localhost:27017/clients') ,  MongooseModule.forFeature([{ name: 'Client', schema: ClientSchema }]) ],
+  imports: [   MongooseModule.forRoot(process.env.MONGO_URI) ,  MongooseModule.forFeature([{ name: 'Client', schema: ClientSchema }]) ],
   controllers: [AppController],
   providers: [AppService],
 })
--- microservices/client/src/main.ts
+++ microservices/client/src/main.ts
@@ -2,13 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common';
 import { Transport  } from '@nestjs/microservices';
-
+import { config } from 'dotenv' ;
+config();
 const logger  = new Logger('Client Service');
 async function bootstrap() {
   const app = await NestFactory.createMicroservice( AppModule , {
     transport: Transport.REDIS,
     options: {
-      url: 'redis://localhost:6379',
+      url: process.env.REDIS_URI,
     },
   });
 
